a970bddbc786eac6fbfde69fefe3867c8c395859
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-19 19:19:05 +0530

    Python Basics

---
+++ intro.py
@@ -0,0 +1,6 @@
+my_message = "bobby's World"
+print(len(my_message))
+
+message = 'Python'
+print(message[0:4])
+  
---
+++ syntax.py
@@ -0,0 +1,22 @@
+#induntation
+
+"""
+if 10 > 2:
+    print("Yes")
+else:
+    print('No')
+
+if 10 < 5:
+    print("Yes")
+else:
+    print('No')"""
+
+
+
+greeting = 'Hello'
+name = 'michael'
+#message = greeting + ', ' + name + '. welcome!'
+message = '{}, {}. Welcome!'.format(greeting, name)
+print(message)
+
+
